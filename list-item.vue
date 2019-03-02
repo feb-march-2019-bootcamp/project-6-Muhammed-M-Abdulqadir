5cc499b670990a04b63e2086f765991307f68b01
Muhamed <mohamet94@gmail.com>
2019-03-03 02:29:45 +0300

    6th project

---
+++ list-item.vue
@@ -0,0 +1,35 @@
+ <template>
+  <li @click="toggleClass" :class="{'active': isActive}">
+    <slot></slot>
+  </li>
+</template>
+ <script>
+export default {
+  data() {
+    return {
+      items: [
+        { description: "Web Development", price: 300 },
+        { description: "Design", price: 400 },
+        { description: "Integration", price: 500 },
+        { description: "Training", price: 200 }
+      ],
+      total: 0,
+      isActive: false
+    };
+  },
+
+  methods: {
+    toggleClass() {
+      this.isActive = !this.isActive;
+
+      this.items.forEach(element => {
+        if (this.isActive) {
+          this.total = this.total + element.price;
+        } else {
+          this.total = this.total - element.price;
+        }
+      });
+    }
+  }
+};
+</script>
---
+++ vue.js
@@ -0,0 +1,12 @@
+import Vue from "vue/dist/vue"
+
+import item from './list-item.vue'
+
+Vue.component('item', item)
+
+new Vue({
+    
+    el: '#app',
+    
+
+});
